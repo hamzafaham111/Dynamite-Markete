57ea9f5d62c323271855cf4cb6c9df2712e7adbb
usman uncle fixerstaff <ufstaff0@gmail.com>
2021-08-28 13:15:12 +0500

    changea

--- src/Components/Services/Products.js
+++ src/Components/Services/Products.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import Products from './SingleProduct';
+import Product from './SingleProduct';
 import Data from './CardsApi';
 import { Container, Grid, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import useStyle from './OurProductsStyle';
 
+
+
 const OurProducts = () => {
+
+    useEffect(() => {
+        this.fetchProducts();
+        console.log('mount it!');
+      }, []);
+    
+
+
     const classes = useStyle();
     return (
         <div >
@@ -22,7 +32,7 @@ const OurProducts = () => {
                            return(
                                <Grid item xs={12} sm={6} lg={3} >
                                    <Link className={classes.link} to="detail" >
-                                <Products 
+                                <Product
                                 name={data.name} 
                                 price={data.Price}
                                 image = {data.Image}/>
